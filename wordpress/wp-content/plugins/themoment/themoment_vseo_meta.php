094dc09efdfd6dcb6fe0e786e0c583c15af508eb
refazul-refat <refazul.refat@gmail.com>
2019-12-02 00:49:00 +0600

    write vseo meta to real post

--- wordpress/wp-content/plugins/themoment/themoment_vseo_meta.php
+++ wordpress/wp-content/plugins/themoment/themoment_vseo_meta.php
@@ -2,34 +2,47 @@
 if (is_page() || is_single()) {
     global $post;
     $post_id = $post->ID;
+    $post_url = get_permalink($post_id);
     $post_vseo_meta = get_post_meta($post_id, 'vseo_meta');
-    $translated_post_vseo_meta = '
-    <script type="application/ld+json">
-        {
-            "@context": "http://schema.org/",
-            "@type": "VideoObject",
-            "name": "Cat video",
-            "duration": "P10M",
-            "uploadDate": "2019-07-19",
-            "thumbnailUrl": "http://www.example.com/cat.jpg",
-            "description": "Watch this cat jump over a fence!",
-            "contentUrl": "http://www.example.com/cat_video_full.mp4",
-            "hasPart": [{
-                    "@type": "Clip",
-                    "name": "Cat jumps",
-                    "startOffset": 30,
-                    "endOffset": 45,
-                    "url": "http://www.example.com/example?t=30"
-                },
-                {
-                    "@type": "Clip",
-                    "name": "Cat misses the fence",
-                    "startOffset": 111,
-                    "endOffset": 150,
-                    "url": "http://www.example.com/example?t=111"
+    foreach ($post_vseo_meta as $meta) {
+        try {
+            $meta = json_decode($meta, true);
+            $playlist_id = $meta['playlist_id'];
+            $playlist_title = $meta['playlist_title'];
+            if (isset($meta['clips'])) {
+                $clip_meta_all = array();
+                $thumbnails = array();
+                foreach ($meta['clips'] as $clip) {
+                    $thumbnails[] = $clip['moment_thumb'];
+                    $clip_meta_all[] = array(
+                        "@type" => "Clip",
+                        "name" => $clip['moment_tag'],
+                        "startOffset" => $clip['moment_time_start'],
+                        "endOffset" => $clip['moment_time_end'],
+                        "url" => $clip['moment_cust_url'] . (strpos($clip['moment_cust_url'], '?') > 0 ? '&' : '?') . 'playlist=' . $playlist_id . '&moment=' . $clip['moment_id']
+                    );
                 }
-            ]
-        }
-    </script>';
-    echo $translated_post_vseo_meta;
+                $clip_meta_all = json_encode($clip_meta_all);
+                $thumbnails = json_encode($thumbnails);
+
+                $translated_post_vseo_meta = sprintf(
+                    '<script type="application/ld+json">
+                        {
+                            "@context": "http://schema.org/",
+                            "@type": "VideoObject",
+                            "name": %s,
+                            "thumbnailUrl": %s,
+                            "contentUrl": %s,
+                            "hasPart": %s
+                        }
+                    </script>',
+                    $playlist_title,
+                    $thumbnails,
+                    $post_url,
+                    $clip_meta_all
+                );
+                echo $translated_post_vseo_meta;
+            }
+        } catch (Exception $e) { }
+    }
 }
