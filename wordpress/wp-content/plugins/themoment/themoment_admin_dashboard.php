4b02902ff0979acc19ef1d8af5b1162ca6eea4c2
refazul-refat <refazul.refat@gmail.com>
2020-02-01 23:34:54 +0600

    setting_page height fix (hardcoded)

--- wordpress/wp-content/plugins/themoment/themoment_admin_dashboard.php
+++ wordpress/wp-content/plugins/themoment/themoment_admin_dashboard.php
@@ -1,1 +1,33 @@
-<iframe src="https://streameditor.tv/use_case_vseo.php" width="100%" style="position: absolute; top: 0px; left: 0px; height: 100%;"></iframe>
+<style type="text/css">
+    #wpcontent, #wpbody {
+        position: initial !important;
+    }
+    .themoment_iframe {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        height: 100%;
+        width: 100%
+    }
+    @media only screen and (min-width: 782px) {
+        .themoment_iframe {
+            left: 160px;
+            width: calc(100% - 160px);
+        }
+        .auto-fold .themoment_iframe {
+            left: 36px;
+            width: calc(100% - 36px);
+        }
+    }
+    @media only screen and (min-width: 960px) {
+        .themoment_iframe {
+            left: 160px;
+            width: calc(100% - 160px);
+        }
+        .auto-fold .themoment_iframe {
+            left: 160px;
+            width: calc(100% - 160px);
+        }
+    }
+</style>
+<iframe class="themoment_iframe" src="https://streameditor.tv/use_case_vseo.php"></iframe>
