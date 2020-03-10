1cb9db074fa56e55e3de812239fab3dc05fe7261
refazul-refat <refazul.refat@gmail.com>
2020-03-10 19:28:02 +0600

    wordpress_menu history

--- wordpress/wp-content/plugins/themoment/themoment_page_history.php
+++ wordpress/wp-content/plugins/themoment/themoment_page_history.php
@@ -1,2 +1,1 @@
-<link rel="stylesheet" href="<?php echo THEMOMENT_PLUGIN_CSS; ?>" />
-<iframe class="themoment_iframe" src="https://themoment.tv/?noredir=1"></iframe>
+<div class="themoment_embedded" data-ovr_mode="ovrdir"></div>
